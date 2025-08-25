1613e086a1fc7793735469464f0748ef7621a5a0
Dev Dixit <devdixitsocial@gmail.com>
2025-08-25 10:37:23 +0530

    solve: scroll to top issue #1

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ import StudyAbroad from "./pages/StudyAbroad";
 import Recruitment from "./pages/Recruitment";
 import Contact from "./pages/Contact";
 import NotFound from "./pages/NotFound";
+import { ScrollToTop } from "./components/ScrollToTop";
 
 const queryClient = new QueryClient();
 
@@ -21,6 +22,7 @@ const App = () => (
         <Toaster />
         <Sonner />
         <BrowserRouter>
+        <ScrollToTop />
           <Routes>
             <Route path="/" element={<Index />} />
             <Route path="/about" element={<About />} />
